d979fd3cc366222791b8256f51e73728669e5981
unknown <AlessandroS@LONLAP08407.eidos.com>
2015-09-24 18:30:39 +0100

    Code to get the word count for one artist

--- main-script.py
+++ main-script.py
@@ -1,5 +1,8 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
+from collections import Counter
+import json
+import unicodedata
 
 def getAuthor(author):
     '''
@@ -46,6 +49,36 @@ def getLinks(soup, author):
                     
     return links
 
+def cleanString(string):
+    cleaned_string = string.replace('<p>','').replace('</p>','').replace('<br/>','') \
+        .replace('\n',' ').replace('\r','').replace('’',' ').replace(',',' ')\
+        .replace("'",' ').replace("(",'').replace(")",'').replace("!",'')\
+        .replace("“",'').replace("”",'').replace("?",'').replace("‘",'')\
+        .replace("#",'').replace("\"",'').replace(".",'').replace('-','')\
+        .replace('*','').replace('&amp;','').replace("/","").replace("+","")\
+        .replace("%","").replace("[","").replace("]","")
+    return cleaned_string
+
+def getLyrics(link):
+    '''
+    Given a link return the lyrics
+    '''
+    #Create a request object
+    url = 'http://www.raptxt.it/'+link
+    headers = {
+        'User-Agent' : r'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7',
+    } 
+    request = Request(url, None, headers)
+    
+    #Make the request
+    data = urlopen(request).read()
+    soup = BeautifulSoup(data, "html.parser")
+    lyrics = soup.find_all("section", class_="testo")[0].p
+    clean_lyrics = cleanString(str(lyrics).lower())
+    
+    #Return the HTML page with all the links
+    return clean_lyrics    
+
 if __name__ == "__main__":
     #Specify the author(s)
     author = "marracash"
@@ -55,3 +88,16 @@ if __name__ == "__main__":
     
     #Parse the HTML page and extract links
     links = getLinks(txt, author)
+    
+    #Iterate the links and extract lyrics
+    lyrics = ''
+    for link in links:
+        lyrics = getLyrics(link)+" "+lyrics
+    
+    #First link
+    #lyrics = getLyrics(links[4])
+    print(lyrics)
+    
+    results = Counter(lyrics.split(" "))
+    #print(json.dumps(results, sort_keys = True, indent = 4, ensure_ascii=False))
+    print(json.dumps(sorted(results.items(), key=lambda item: item[1]), indent = 4, ensure_ascii=False))
